c25ca803fdeb45690b657d774c353832d81740b7
ptorfer <151776139+ptorfer@users.noreply.github.com>
2023-11-23 00:17:29 +0100

    Create Rey.java
    Creación atributos de la clase Rey.

---
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
@@ -0,0 +1,9 @@
+package org.iesalandalus.programacion.reyajedrez.modelo;
+
+public class Rey {
+    //Creación de atributos
+    private Color color; //pongo en minuscula color y posicion porque se nombran
+                         //en minúscula los atributos
+    private Posicion posicion;
+    private int totalMovimientos;
+}
