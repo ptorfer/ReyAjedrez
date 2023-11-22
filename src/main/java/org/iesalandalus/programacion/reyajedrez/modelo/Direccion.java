bfbd84ed2962185069800cbc52d6da2985795faa
ptorfer <151776139+ptorfer@users.noreply.github.com>
2023-11-22 20:30:48 +0100

    Create Direccion.java
    Creado enumerado llamado Direccion.

---
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Direccion.java
@@ -0,0 +1,16 @@
+package org.iesalandalus.programacion.reyajedrez.modelo;
+
+public enum Direccion {
+        public enum Direccion {
+        NORTE("NORTE"),
+        NORESTE("NORESTE"),
+        ESTE("ESTE"),
+        SURESTE"SURESTE",
+        SUR("SUR"),
+        SUROESTE("SUROESTE"),
+        OESTE("OESTE"),
+        NOROESTE("NOROESTE"),
+        ENROQUE_CORTO("ENROQUE_CORTO"),
+        ENROQUE_LARGO("ENROQUE_LARGO");
+    }
+}
