8f1811c79a9a2ae6ca0a5747b9c7cf10734691db
ptorfer <151776139+ptorfer@users.noreply.github.com>
2023-11-22 22:29:24 +0100

    Update Posicion.java
    Creación de método set y get dentro de clase Posicion.

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
@@ -1,7 +1,36 @@
 package org.iesalandalus.programacion.reyajedrez.modelo;
 
 public class Posicion {
+    //Creación de atributos
     private int fila;
     private char columna;
 
+    //Creación método set de filas y columnas
+    public void setfila(int fila) {
+        if (fila > 0 || fila < 9) {
+            this.fila = fila;
+        } else {
+            throw new IllegalArgumentException("Por favor introduce un número del 1 " +
+                    "al 8.");
+        }
+    }
+    public void setcolumna(char columna) {
+        if (columna >= 'a' || columna <= 'h') {
+            this.columna = columna;
+        } else {
+            throw new IllegalArgumentException(("Por favor introduce una letra de " +
+                    "la a a la h.");
+        }
+    }
+
+    //Creación método get de filas y columnas
+    public int getfila() {
+        return fila;
+    }
+
+    public char getcolumna() {
+        return columna;
+    }
 }
+
+
