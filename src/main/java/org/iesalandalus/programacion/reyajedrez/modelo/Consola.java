323c361c93419eb1c6fd4e89d097708c295f94b3
ptorfer <151776139+ptorfer@users.noreply.github.com>
2023-11-30 23:00:34 +0100

    Update Consola.java
     constructor para esta clase con su visibilidad adecuada, teniendo en cuenta que será una clase de utilidades que sólo contendrá métodos estáticos.

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Consola.java
@@ -2,5 +2,10 @@ package org.iesalandalus.programacion.reyajedrez.modelo;
 
 public class Consola {
 
-    
+    /*constructor para esta clase con su visibilidad adecuada, teniendo en cuenta
+    que será una clase de utilidades que sólo contendrá métodos estáticos.*/
+
+    private Consola() {
+    }
+
 }
