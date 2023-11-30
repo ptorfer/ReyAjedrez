783bad8d60aacc201956df21940eed908c032239
ptorfer <151776139+ptorfer@users.noreply.github.com>
2023-11-30 22:55:29 +0100

    Update Rey.java
    Creación to string

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
@@ -18,20 +18,18 @@ public class Rey {
         this.posicion = Objects.requireNonNull(posicion, "La posición " +
                                                        " del rey no puede ser nula.");
 
-    public int color() {
-        return color;
 
-    public Posicion getPosicion() {
-        return posicion;
+    }
 
     //constructor por defecto para esta clase que cree un rey blanco en la posición "1e"
-    public Rey(){
+    public Rey() {
         color = Color.BLANCO;
-        posicion = new Posicion(1,'e');
+        posicion = new Posicion(1, 'e');
 
     /*constructor para la clase que acepte como parámetro el color y que creará un rey de dicho color
     cuya posición será "1e" si es blanco o "8e" si es negro*/
 
+    }
     public Rey(){
         if (color = Color.BLANCO) {
 
@@ -46,8 +44,13 @@ public class Rey {
 
     }
 
+/*Crea el método toString que devuelva un String que será la representación de dicho objeto (color y posición).
+Realiza un commit.*/
 
-
+    @Override
+    public String toString() {
+        return String.format("Rey[color=%s, posicion=%s]", this.fila, this.columna);
+    }
 
 }
 
