3d79ad6212f7ef0502b64af9ae3b627d581ab161
ptorfer <151776139+ptorfer@users.noreply.github.com>
2023-11-30 23:07:00 +0100

    Update Consola.java
    método mostrarMenu

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Consola.java
@@ -8,4 +8,11 @@ public class Consola {
     private Consola() {
     }
 
+    public static mostrarMenu() {
+        System.out.println("1. Crear rey por defecto.");
+        System.out.println("2. Crear rey eligiendo el color.");
+        System.out.println("3. Mover.");
+        System.out.println("4. Salir.");
+
+    }
 }
